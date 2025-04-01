4dffaffedf585f4a58a279eae4215d7e684eacd5
Nihir14 <nihirverma14@gmail.com>
2025-04-01 12:29:28 +0530

    update

--- CustomLinkedList.java
+++ CustomLinkedList.java
@@ -1,162 +1,54 @@
-import java.util.NoSuchElementException;
+import java.util.SplittableRandom;
 
-public class CustomLinkedList<E> {
-    private Node<E> first;
-    private Node<E> last;
+public class CustomLinkedList {
+
     private int size;
+    private Node first;
+    private Node last;
 
-    private static class Node<E> {
-        E item;
-        Node<E> next;
-        Node<E> prev;
+    private class Node {
 
-        Node(Node<E> prev, E element, Node<E> next) {
-            this.item = element;
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public Node(int value, Node next) {
+            this.value = value;
             this.next = next;
-            this.prev = prev;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
         }
     }
 
-    public CustomLinkedList() {
-        this.size = 0;
-        this.first = null;
-        this.last = null;
-    }
-
-    public void add(E element) {
-        linkLast(element);
-    }
-
-    public void addFirst(E element) {
-        linkFirst(element);
-    }
-
-    public void addLast(E element) {
-        linkLast(element);
-    }
-
-    public E removeFirst() {
-        final Node<E> f = first;
-        if (f == null)
-            throw new NoSuchElementException();
-        return unlinkFirst(f);
-    }
-
-    public E removeLast() {
-        final Node<E> l = last;
-        if (l == null)
-            throw new NoSuchElementException();
-        return unlinkLast(l);
-    }
-
-    public boolean remove(E element) {
-        if (element == null) {
-            for (Node<E> x = first; x != null; x = x.next) {
-                if (x.item == null) {
-                    unlink(x);
-                    return true;
-                }
-            }
+    public boolean add(int value) {
+        Node node = new Node(value);
+        if (last == null) {
+            first = node;
         } else {
-            for (Node<E> x = first; x != null; x = x.next) {
-                if (element.equals(x.item)) {
-                    unlink(x);
-                    return true;
-                }
-            }
+            last.next = node;
         }
-        return false;
-    }
-
-    public E getFirst() {
-        final Node<E> f = first;
-        if (f == null)
-            throw new NoSuchElementException();
-        return f.item;
-    }
-
-    public E getLast() {
-        final Node<E> l = last;
-        if (l == null)
-            throw new NoSuchElementException();
-        return l.item;
-    }
-
-    public int size() {
-        return size;
-    }
-
-    private void linkFirst(E e) {
-        final Node<E> f = first;
-        final Node<E> newNode = new Node<>(null, e, f);
-        first = newNode;
-        if (f == null)
-            last = newNode;
-        else
-            f.prev = newNode;
+        last = node;
         size++;
+
+        return true;
     }
 
-    private void linkLast(E e) {
-        final Node<E> l = last;
-        final Node<E> newNode = new Node<>(l, e, null);
-        last = newNode;
-        if (l == null)
-            first = newNode;
-        else
-            l.next = newNode;
-        size++;
-    }
 
-    private E unlinkFirst(Node<E> f) {
-        final E element = f.item;
-        final Node<E> next = f.next;
-        f.item = null;
-        f.next = null;
-        first = next;
-        if (next == null)
-            last = null;
-        else
-            next.prev = null;
-        size--;
-        return element;
-    }
-
-    private E unlinkLast(Node<E> l) {
-        final E element = l.item;
-        final Node<E> prev = l.prev;
-        l.item = null;
-        l.prev = null;
-        last = prev;
-        if (prev == null)
-            first = null;
-        else
-            prev.next = null;
-        size--;
-        return element;
-    }
-
-    private E unlink(Node<E> x) {
-        final E element = x.item;
-        final Node<E> next = x.next;
-        final Node<E> prev = x.prev;
-
-        if (prev == null) {
-            first = next;
-        } else {
-            prev.next = next;
-            x.prev = null;
-        }
-
-        if (next == null) {
-            last = prev;
-        } else {
-            next.prev = prev;
-            x.next = null;
-        }
-
-        x.item = null;
-        size--;
-        return element;
-    }
 }
